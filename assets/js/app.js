ff42f3fae66af2ac1730de23eb429c1f3e51f62c
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-02-23 11:46:24 -0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
-console.log("Hello!, world")
+document.addEventListener('DOMContentLoaded', () => {
+    const elementCard = document.getElementsByClassName('card-container');
+    let turn = 0;
+
+    elementCard[0].addEventListener('click', () => {
+
+        if (turn == 0) {
+            elementCard[0].classList.add('flip');
+            turn = 1;
+        } else {
+            elementCard[0].classList.remove('flip');
+            turn = 0;
+        }
+
+    })
+    
+})
