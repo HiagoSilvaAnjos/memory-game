a9bfeca4bfe393936ddcd4247c05477a72cb7c30
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-02-25 16:54:16 -0300

    adicionando as cartas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 const cardFront = 'card-front';
 const cardBack = 'card-back';
+const classCard = 'card';
+const iconCard = 'icon';
 let iconsTechs = [
     'bootstrap',
     'electron',
@@ -15,13 +17,54 @@ let iconsTechs = [
 
 let cards = null;
 
-startGame()
+startGame();
 
 function startGame() {
-    
     cards = createCardsFromTemplate(iconsTechs);
     shuffleCards(cards);
-    console.log(cards);
+    initializeCards(cards);
+}
+
+// Inicializar cartas
+function initializeCards(cards) {
+    let gameBoard = document.querySelector('.board-container');
+
+    cards.forEach((card) => {
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(classCard);
+        cardElement.dataset.icon = card.icon;  
+        
+        createCardContent(card, cardElement);
+        
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+    })
+}
+
+// criar conteudo das cartas
+function createCardContent(card, cardElement) {
+    
+    createCardFace(cardFront, card, cardElement);
+    createCardFace(cardBack, card, cardElement);
+
+}
+
+function createCardFace (face, card, element) {
+
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+
+    if (face === cardFront) {
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(iconCard);
+        iconElement.src = `./assets/images/${card.icon}.png`
+
+        cardElementFace.appendChild(iconElement);
+    } else {
+        cardElementFace.innerHTML = '<p>&lt&#47&gt</p>';
+    }
+    element.appendChild(cardElementFace);
 }
 
 function shuffleCards(cards) {
@@ -40,12 +83,11 @@ function shuffleCards(cards) {
 function createCardsFromTemplate (iconsTechs) {
     let cards = [];
 
-    for (let i of iconsTechs) {
-        cards.push(createPairFromTemplate(i))
-    }
+    iconsTechs.forEach(function (tech) {
+        cards.push(createPairFromTemplate(tech))
+    })
 
     return cards.flatMap(pair => pair)
-
 }
 
 // criar os pares
@@ -63,6 +105,12 @@ function createPairFromTemplate (tech) {
     }]
 }   
 
+// Cria o id da carta
 function createIdWithTemplate (tech) {
    return tech + parseInt((Math.random() * 1000));
+}
+
+// virar carta
+function flipCard() {
+    this.classList.add("flip");
 }
